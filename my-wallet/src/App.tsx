d322799c781698e68655d4e636750cf40be4c352
Rafhael Gomes <rafhaelgomes@iMac-de-Rafhael.local>
2022-08-04 18:29:44 -0300

    estrutura do projeto

--- my-wallet/src/App.tsx
+++ my-wallet/src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div>
+    <header>
+      <h1>Meu controle de despesas</h1>
+    </header>
+    <section>
+      ...
+    </section>
+  </div>
+  )
 }
 
 export default App;
